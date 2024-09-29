2aadc1a99eb7a9aa0d0b438cd8afb99af70efbbe
alex <alexnasiali45@gmail.com>
2024-09-29 09:58:05 +0300

    First commit

---
+++ speed.js
@@ -0,0 +1,17 @@
+function checkSpeed(speed) {
+    const speedLimit = 70;  //the constant speedLimit
+    const  kmS = 5;
+    if (speed < speedLimit) {
+        console.log("OK"); //when speed is not over 70 "OK" is displayed
+    }else{
+        const demeritPoints = math.floor ((speed -speedLimit) / kmS); //to calculate demerit pooints 
+        if (demeritPoints > 12) {
+            console.log("LICENCE SUSPENDED!!"); //output when demerit points exceed 12
+        }else{
+            console.log(`points: ${demeritPoints}`);
+        };
+    };
+};
+
+
+checkSpeed(); //callingBack the function
