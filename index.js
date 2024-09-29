343e02ab3682315744ff4fd6d7cb8857504dc2c4
alex <alexnasiali45@gmail.com>
2024-09-29 09:57:12 +0300

    First commit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+// (to detemine student grade the code below will output grade according to the input inserted)
 function grades(mark) {
   if (mark > 79) {
     return "A";
@@ -11,15 +13,17 @@ function grades(mark) {
     return "E";
   }
 }
+// promts the user to input students mark so that it can be graded
 function input() {
   let mark = prompt("Student mark (0-100):");
   mark = parseFloat(mark);
-  if (!isNaN(mark) && mark >= 0 && mark <= 100) {
-    let grade = grades(mark);
-    alert(`Grade: ${grade}`);
+  if (!isNaN(grades) && mark >= 0 && mark <= 100) {
+    let grade = (grades(mark));
+    alert(`Grade: ${grades}`);
   } else {
-    alert("NOT VALID!!!");
+    alert("error message: NOT VALID!!!");// appears when the mark is not between 0 to 100
   }
 }
 
-console.log(input);
+console.log(input); //callingback the function
+
