d915ee2536c7273bb155f607e0371e93f6fdf8f9
alex <alexnasiali45@gmail.com>
2024-09-29 20:37:11 +0300

    new

--- speed.js
+++ speed.js
@@ -2,7 +2,7 @@ function checkSpeed(speed) {
     const speedLimit = 70;  //the constant speedLimit
     const  kmS = 5;
     if (speed < speedLimit) {
-        console.log("OK"); //when speed is not over 70 "OK" is displayed
+        console.log("OK"); //when speed is not over 70 prints "OK"
     }else{
         const demeritPoints = math.floor ((speed -speedLimit) / kmS); //to calculate demerit pooints 
         if (demeritPoints > 12) {
